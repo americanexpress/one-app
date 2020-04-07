88d2a2a85d0ac4f6e477a7f7e3eea0f00cced2cc
nkiboi <nelly.j.kiboi@aexp.com>
2020-04-07 11:45:31 +0100

    chore(actions): updated as per pr reviews

--- scripts/run-integration-tests.travis.sh
+++ scripts/run-integration-tests.travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 docker build -t one-app:at-test .
-if [[ "${TRAVIS_BRANCH}" = "prerelease" ]] ||
+if [[ "${TRAVIS_BRANCH}" = "prepare-release" ]] ||
 [[ "${TRAVIS_BRANCH}" = "master"  && "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
   npm run build:sample-modules -- --archive-built-artifacts --bundle-statics-origin=https://one-app-statics.surge.sh
   echo 'https://one-app-sample.herokuapp.com' >> sample-module-bundles/CORS && \
