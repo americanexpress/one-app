9077f17f91370f72c7ce1b5da5a7e08aae4fe259
Mike Tobia <Francois-Esquire@users.noreply.github.com>
2020-05-14 18:03:51 -0400

    feat(pwa/web-manifest): add middleware & config support

--- __tests__/server/middleware/pwa/validation.spec.js
+++ __tests__/server/middleware/pwa/validation.spec.js
@@ -51,20 +51,140 @@ describe('validation', () => {
     expect(validatePWAConfig({
       serviceWorker: 'true',
       scope: 42,
+      webManifest: [],
     })).toEqual({});
-    expect(console.warn).toHaveBeenCalledTimes(2);
-    expect(console.warn).toHaveBeenCalledWith('invalid value type given for configuration key "serviceWorker" (expected "Boolean") - ignoring');
-    expect(console.warn).toHaveBeenCalledWith('invalid value type given for configuration key "scope" (expected "String") - ignoring');
+    expect(console.warn).toHaveBeenCalledTimes(3);
+    expect(console.warn).toHaveBeenCalledWith('Invalid value type given for configuration key "serviceWorker" (expected "Boolean") - ignoring');
+    expect(console.warn).toHaveBeenCalledWith('Invalid value type given for configuration key "scope" (expected "String") - ignoring');
+    expect(console.warn).toHaveBeenCalledWith('Invalid value type given for configuration key "webManifest" (expected "WebManifest") - ignoring');
   });
 
   test('valid keys emits no warnings or errors and returns valid configuration', () => {
     const validConfig = {
       serviceWorker: true,
       scope: '/',
+      webManifest: { name: 'my-app' },
     };
 
     expect(validatePWAConfig(validConfig)).toEqual(validConfig);
     expect(console.warn).not.toHaveBeenCalled();
     expect(console.error).not.toHaveBeenCalled();
+  });
+
+  describe('web manifest validation', () => {
+    test('web app manifest has valid keys emits no warnings or errors and returns valid configuration', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          name: 'One App Test',
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual(validConfig);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+    });
+
+    test('expects name to be required', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          short_name: 'One App Test',
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual(validConfig);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
+
+    test('ignores unrecognized keys given', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          name: 'One App Test',
+          my_name: 'One App Test',
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual({ ...validConfig, webManifest: { name: 'One App Test' } });
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect(console.error).not.toHaveBeenCalled();
+    });
+
+    test('warns and ignores when invalid enumerable keys are used', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          name: 'One App Test',
+          display: 'big',
+          orientation: 'up',
+          direction: 'backwards',
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual({ ...validConfig, webManifest: { name: 'One App Test' } });
+      expect(console.warn).toHaveBeenCalledTimes(3);
+      expect(console.error).not.toHaveBeenCalled();
+    });
+
+    test('warns and ignores when invalid shapes for array are used', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          name: 'One App Test',
+          icons: [{
+            size: '72x72',
+          }, {
+            purpose: 'none',
+          }],
+          screenshots: [{
+            href: 'https://screenshots.example.com/screenshot/latest',
+          }],
+          related_applications: [{
+            store: 'new pwa store',
+          }],
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual({ ...validConfig, webManifest: { name: 'One App Test' } });
+      expect(console.warn).toHaveBeenCalledTimes(3);
+      expect(console.error).not.toHaveBeenCalled();
+    });
+
+    test('includes a valid web manifest when passed in', () => {
+      const validConfig = {
+        serviceWorker: true,
+        scope: '/',
+        webManifest: {
+          lang: 'en-US',
+          dir: 'auto',
+          display: 'standalone',
+          orientation: 'portrait',
+          short_name: 'Test',
+          name: 'One App Test',
+          categories: ['testing', 'example'],
+          icons: [{
+            src: 'https://example.com/pwa-icon.png',
+            type: 'img/png',
+            sizes: '72x72',
+            purpose: 'badge',
+          }],
+          screenshots: [{
+            src: 'https://example.com/pwa-screenshot.png',
+            type: 'img/png',
+            sizes: '1024x768',
+          }],
+          related_applications: [{
+            platform: 'new pwa store',
+            url: 'https://platform.example.com/pwa',
+            id: 'aiojfoahfaf',
+          }],
+        },
+      };
+      expect(validatePWAConfig(validConfig)).toEqual(validConfig);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+    });
   });
 });
