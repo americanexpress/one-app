9f7775f654ad6deabf21973aac4f4a3ba2df2eb8
Mike Tobia <michael.m.tobia@aexp.com>
2020-05-29 16:46:05 -0400

    fix(samples): build

--- prod-sample/build-cdn.sh
+++ prod-sample/build-cdn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-time {
-  rm -rf prod-sample/nginx/origin-statics
-  mkdir prod-sample/nginx/origin-statics
-  cp -R prod-sample/assets/ prod-sample/nginx/origin-statics
-  cp -R static/ prod-sample/nginx/origin-statics
-}
+
+rm -rf prod-sample/nginx/origin-statics
+mkdir prod-sample/nginx/origin-statics
+cp -R prod-sample/assets/ prod-sample/nginx/origin-statics
+cp -R static/ prod-sample/nginx/origin-statics
--- prod-sample/build-sample-modules.sh
+++ prod-sample/build-sample-modules.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-time {
-  for dir in prod-sample/sample-modules/*/
+for dir in prod-sample/sample-modules/*/
+do
+  for path in $dir*/
   do
-    for path in $dir*/
-    do
-      path=${path%*/}
+    path=${path%*/}
 
-      echo "installing $path"
-      (cd $path && NODE_ENV=development npm ci)
+    echo "installing $path"
+    (cd $path && NODE_ENV=development npm ci)
 
-      version=${path##*/}
-      if [[ "$version" =~ "0.0.0" ]]; then
-        echo "serving $version"
-        npm run serve-module $path
-      fi
-    done
+    version=${path##*/}
+    if [[ "$version" =~ "0.0.0" ]]; then
+      echo "serving $version"
+      npm run serve-module $path
+    fi
   done
-}
+done
