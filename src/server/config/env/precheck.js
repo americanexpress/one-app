febb255bacef8a6b2c8ee11a3afd5ace3b08a64d
Salem Naim <salem.naim@aexp.com>
2021-01-18 10:45:59 -0500

    feat(): add rest of rules

--- src/server/config/env/precheck.js
+++ src/server/config/env/precheck.js
@@ -1,41 +1,164 @@
 import { argv } from 'yargs';
+import isFetchableUrlInNode from '@americanexpress/env-config-utils/isFetchableUrlInNode';
+import isFetchableUrlInBrowser from '@americanexpress/env-config-utils/isFetchableUrlInBrowser';
+import path from 'path';
 
 // The required environment variables and config options.
 const environment = {
   NODE_ENV: process.env.NODE_ENV,
   rootModuleName: argv.rootModuleName,
-
+  HOLOCRON_MODULE_MAP_URL: process.env.HOLOCRON_MODULE_MAP_URL,
+  ONE_CLIENT_REPORTING_URL: process.env.ONE_CLIENT_REPORTING_URL,
+  ONE_CLIENT_CSP_REPORTING_URL: process.env.ONE_CLIENT_CSP_REPORTING_URL,
+  ONE_CLIENT_ROOT_MODULE_NAME: process.env.ONE_CLIENT_ROOT_MODULE_NAME,
+  ONE_CLIENT_CDN_URL: process.env.ONE_CLIENT_CDN_URL,
+  ONE_CONFIG_ENV: process.env.ONE_CONFIG_ENV,
+  localModuleMap: argv.localModuleMap,
+  rootModuleNameDuplicate: Boolean(argv.rootModuleName && process.env.ONE_CLIENT_ROOT_MODULE_NAME),
 };
 
 // The rules the environment must follow
 const rules = {
   NODE_ENV(parameter) {
-    const validation = {
+    return {
       success: ['development', 'production']
         .some((option) => option === parameter),
-      explanation: `Environment variable NODE_ENV must be either "development" or "production"; given: "${parameter}"`,
+      explanation: `NODE_ENV environment variable must be either "development" or "production"; given: ${parameter}`,
     };
-    return validation;
   },
   rootModuleName(parameter) {
-    const validation = {
+    return {
       success: Boolean(parameter) === true,
-      explanation: 'option --root-module-name=<module-name> must be supplied if NODE_ENV != "development"',
+      explanation: 'option --root-module-name=<module-name> must be defined if NODE_ENV != "development"',
     };
-    return validation;
+  },
+  HOLOCRON_MODULE_MAP_URL(parameter) {
+    let success = true;
+    let message;
+    try {
+      isFetchableUrlInNode(parameter);
+    } catch (e) {
+      success = false;
+      message = e.message;
+    }
+    return {
+      success,
+      explanation: `HOLOCRON_MODULE_MAP_URL: ${message}`,
+    };
+  },
+  ONE_CLIENT_REPORTING_URL(parameter) {
+    let success = true;
+    let message;
+    try {
+      isFetchableUrlInBrowser(parameter);
+    } catch (e) {
+      success = false;
+      message = e.message;
+    }
+    return {
+      success,
+      explanation: `ONE_CLIENT_REPORTING_URL: ${message}`,
+    };
+  },
+  ONE_CLIENT_CSP_REPORTING_URL(parameter) {
+    let success = true;
+    let message;
+    try {
+      isFetchableUrlInBrowser(parameter);
+    } catch (e) {
+      success = false;
+      message = e.message;
+    }
+    return {
+      success,
+      explanation: `ONE_CLIENT_CSP_REPORTING_URL: ${message}`,
+    };
+  },
+  ONE_CLIENT_ROOT_MODULE_NAME(parameter) {
+    return {
+      success: Boolean(parameter) === true,
+      explanation: 'ONE_CLIENT_ROOT_MODULE_NAME environment variable must be defined',
+    };
+  },
+  ONE_CLIENT_CDN_URL(parameter) {
+    let success = true;
+    let message;
+    try {
+      isFetchableUrlInBrowser(parameter);
+      if (!/\/$/.test(parameter)) {
+        throw new Error('ONE_CDN_URL must have a trailing slash');
+      }
+    } catch (e) {
+      success = false;
+      message = e.message;
+    }
+    return {
+      success,
+      explanation: `ONE_CDN_URL: ${message}`,
+    };
+  },
+  ONE_CONFIG_ENV(parameter) {
+    return {
+      success: Boolean(parameter) === true,
+      explanation: 'ONE_CONFIG_ENV environment variable must be defined',
+    };
+  },
+  localModuleMap(parameter) {
+    // only require a remote module map if there are no locally served modules
+    let isLocalModuleMapEmpty = false;
+    try {
+      // eslint-disable-next-line import/no-dynamic-require,global-require
+      const localModuleMap = require(path.join(process.cwd(), 'static', 'module-map.json'));
+      if (Object.entries(localModuleMap).length === 0) {
+        isLocalModuleMapEmpty = true;
+      }
+      // if local module map is an empty file we need to catch it
+    } catch (error) {
+      isLocalModuleMapEmpty = true;
+    }
+    return {
+      success: !isLocalModuleMapEmpty || Boolean(parameter) === true,
+      explanation: '`module-map-url` is required if there are no locally served modules',
+    };
+  },
+  rootModuleNameDuplicate(parameter) {
+    return {
+      success: !parameter,
+      explanation: 'Both the `root-module-name` argument and the `ONE_CLIENT_ROOT_MODULE_NAME` environment variable have been provided, but only one may be set at once.\n',
+    };
   },
 };
 
 
 function selectRules() {
-  const { rootModuleName, NODE_ENV } = rules;
+  const {
+    NODE_ENV,
+    rootModuleName,
+    HOLOCRON_MODULE_MAP_URL,
+    ONE_CLIENT_REPORTING_URL,
+    ONE_CLIENT_CSP_REPORTING_URL,
+    ONE_CLIENT_ROOT_MODULE_NAME,
+    ONE_CLIENT_CDN_URL,
+    ONE_CONFIG_ENV,
+    localModuleMap,
+    rootModuleNameDuplicate,
+  } = rules;
   if (environment.NODE_ENV === 'development') {
     return {
       rootModuleName,
+      localModuleMap,
     };
   }
   if (environment.NODE_ENV === 'production') {
-    return {};
+    return {
+      HOLOCRON_MODULE_MAP_URL,
+      ONE_CLIENT_REPORTING_URL,
+      ONE_CLIENT_CSP_REPORTING_URL,
+      ONE_CLIENT_ROOT_MODULE_NAME,
+      ONE_CLIENT_CDN_URL,
+      ONE_CONFIG_ENV,
+      rootModuleNameDuplicate,
+    };
   }
   return {
     NODE_ENV,
