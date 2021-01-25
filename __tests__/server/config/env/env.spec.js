e797617e7c4c997d371e16a7d6197a9cd90a7b28
Salem Naim <salem.naim@aexp.com>
2021-01-25 16:37:51 -0500

    fix(): catch error in env.spec

--- __tests__/server/config/env/env.spec.js
+++ __tests__/server/config/env/env.spec.js
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2021 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 import path from 'path';
 
 jest.mock('yargs', () => ({ argv: { rootModuleName: 'my-module' } }));
-jest.doMock(path.join(process.cwd(), 'static', 'module-map.json'), () => {
-  throw new Error('bad module map path');
-});
+
+try {
+  jest.doMock(path.join(process.cwd(), 'static', 'module-map.json'), () => {
+    throw new Error('bad module map path');
+  });
+} catch (e) {
+  jest.fn();
+}
 
 describe('env', () => {
   test('moduleMap field is undefined if module map path non-existent', () => {
