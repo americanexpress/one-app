f00f4819a2b1ab1b335e1f6f5d6062b44dc1ac41
Giuliano Kranevitter <giuliano.kranevitter1@aexp.com>
2022-11-08 15:10:49 -0500

    chore: modified

--- src/server/plugins/addCacheHeaders.js
+++ src/server/plugins/addCacheHeaders.js
@@ -14,9 +14,26 @@
  * permissions and limitations under the License.
  */
 
-export default function addCacheHeaders(req, res, next) {
-  res.set('Cache-Control', 'no-store');
-  res.set('Pragma', 'no-cache');
+import fp from 'fastify-plugin';
 
-  next();
-}
+/**
+ * Fastify Plugin that adds header cache configuration
+ * @param {import('fastify').FastifyInstance} fastify Fastify instance
+ * @param {import('fastify').FastifyPluginOptions} _opts plugin options
+ * @param {import('fastify').FastifyPluginCallback} done plugin callback
+ */
+const addCacheHeaders = (fastify, _opts, done) => {
+  fastify.addHook('onRequest', async (request, reply) => {
+    if (request.method.toLowerCase() === 'get') {
+      reply.header('Cache-Control', 'no-store');
+      reply.header('Pragma', 'no-cache');
+    }
+  });
+
+  done();
+};
+
+export default fp(addCacheHeaders, {
+  fastify: '4.x',
+  name: 'addCacheHeaders',
+});
