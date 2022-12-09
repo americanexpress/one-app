1346c92ded961d6d92abb06c35aa2cecf4193c5b
dpepin <daniel.pepin1@aexp.com>
2022-12-09 11:47:59 -0500

    feat(fastifycdn): remove comment and change license year

--- src/server/utils/devCdnFactory.js
+++ src/server/utils/devCdnFactory.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2022 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express
+ * or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 import path from 'path';
 import fs from 'fs';
 import got from 'got';
@@ -151,7 +165,7 @@ export const oneAppDevCdnFactory = ({
       .code(200)
       .send(map);
   });
-  // We dont return a value here so disable eslint
+
   // eslint-disable-next-line consistent-return
   oneAppDevCdn.get('*', async (req, reply) => {
     const incomingRequestPath = req.url;
