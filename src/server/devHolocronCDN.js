8bcd6a1da80d3c7d71bf10b4f5a844a928c08a43
Nickolas Oliver <Nickolas.Oliver@aexp.com>
2023-03-29 03:37:31 -0700

    feat(server): messages of missing local modules in devHolocronCDN developer-friendly issue message and usual situation & resolution

--- src/server/devHolocronCDN.js
+++ src/server/devHolocronCDN.js
@@ -22,8 +22,41 @@ import express from 'express';
 import cors from 'cors';
 import { argv } from 'yargs';
 import oneAppDevCdn from '@americanexpress/one-app-dev-cdn';
+import { register } from 'prom-client';
+
+import { holocron as holocronMetrics } from './metrics';
+import { getClientModuleMapCache } from './utils/clientModuleMapCache';
 
 const hasLocalModuleMap = () => fs.existsSync(path.join(process.cwd(), 'static', 'module-map.json'));
+
+async function calculateSuccessfulModuleMapLoads() {
+  const clientModuleMapCache = getClientModuleMapCache();
+  if (Object.keys(clientModuleMapCache).length < 1) {
+    return 0;
+  }
+
+  const metric = await register.getSingleMetric(holocronMetrics.moduleMapUpdated).get();
+  // single item in the array, no lables
+  return metric.values[0].value + 1;
+};
+
+async function localModuleBundleNotFound(moduleIdentifier) {
+  // same issue, different resolution suggestions based on scenario (starting up? already up?)
+  const issueMessage = `cannot find the bundles for ${moduleIdentifier} on the local filesystem`;
+
+  // if we don't have a successful 1st poll yet the server needs to close with an error
+  const moduleMapLoads = await calculateSuccessfulModuleMapLoads();
+
+  if (moduleMapLoads === 0) {
+    console.error(`${issueMessage}, you probably need to build the module`);
+    // end the startup to avoid confusing behavior
+    // the user controls their local disk so can resolve the need to build
+    // TODO: can we wait until the first load attempt finishes? so the user can see all the modules they need to build at once, rather than iteratively
+    process.exit(2);
+  }
+
+  console.error(`${issueMessage}, it may be in the middle of a build or the build is failing`);
+}
 
 const app = express();
 
@@ -34,6 +67,10 @@ app.use('/static', oneAppDevCdn({
   useLocalModules: hasLocalModuleMap(),
   appPort: process.env.HTTP_PORT,
   useHost: argv.useHost,
+
+  hooks: {
+    localModuleBundleNotFound,
+  },
 }));
 
 export default app;
