a50ae737f964f58fd8002dc3231674628932e306
Matthew Mallimo <matthew.c.mallimo@aexp.com>
2023-10-30 09:29:27 -0400

    feat: update strict transport security header to 2 years

--- src/server/middleware/addSecurityHeaders.js
+++ src/server/middleware/addSecurityHeaders.js
@@ -17,7 +17,7 @@
 export default function addSecurityHeaders(req, res, next) {
   res.set('X-Frame-Options', 'DENY');
   res.set('X-Content-Type-Options', 'nosniff');
-  res.set('Strict-Transport-Security', 'max-age=15552000; includeSubDomains');
+  res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains');
   res.set('X-XSS-Protection', '1; mode=block');
   res.set('Referrer-Policy', process.env.ONE_REFERRER_POLICY_OVERRIDE || 'same-origin');
   next();
