823a12bbac303ac53459fd1f344f1509656088c2
Matthew Mallimo <matthew.c.mallimo@aexp.com>
2023-11-03 14:26:05 -0400

    fix: correctly set strict transport security header (#1182)

--- __tests__/server/middleware/addSecurityHeaders.spec.js
+++ __tests__/server/middleware/addSecurityHeaders.spec.js
@@ -26,7 +26,6 @@ describe('addSecurityHeaders', () => {
     const securityHeaders = {
       'X-Frame-Options': 'DENY',
       'X-Content-Type-Options': 'nosniff',
-      'Strict-Transport-Security': 'max-age=63072000; includeSubDomains',
       'X-XSS-Protection': '1; mode=block',
       'Referrer-Policy': 'same-origin',
     };
