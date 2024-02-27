5a48dd6f0b4320cf1f7b881e00f60f1de245b34e
dpepin <daniel.pepin1@aexp.com>
2024-02-27 10:23:43 -0500

    chore(dep): update franks

--- prod-sample/sample-modules/frank-lloyd-root/0.0.0/src/getIP.js
+++ prod-sample/sample-modules/frank-lloyd-root/0.0.0/src/getIP.js
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2024 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 import { networkInterfaces } from 'os';
 
 export const getIp = () => {
   const interfaces = networkInterfaces();
 
-  const ipAddress = Object.keys(interfaces)
+  const ipAddresses = Object.keys(interfaces)
     .map((name) => interfaces[name].find((iface) => iface.family === 'IPv4'
             && !iface.internal && iface.address !== '127.0.0.1'))
     .filter(Boolean);
 
-  return ipAddress[0] ? ipAddress[0].address : '0.0.0.0';
+  return ipAddresses[0]?.address ?? '127.0.0.1';
 };
